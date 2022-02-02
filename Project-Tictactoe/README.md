91552fd5bad2388f3c23171c03d6cf1c68fcec75
jollylollylion <70806287+jollylollylion@users.noreply.github.com>
2022-02-02 21:46:36 +0000

    Add files via upload
    Added Game Folders

---
+++ Project-Tictactoe/README.md
@@ -0,0 +1,15 @@
+## Project Tic-Tac-Toe
+
+This is the source code for [Project Tic Tac Toe]
+
+### Requirements
+[Python 3.8](https://docs.python.org/3/whatsnew/3.8.html) or later.
+
+### Run the game
+
++ _install third-party packages and invoke as a module_
+
+```bash
+$ pip install -r requirements.txt
+$ python -m tictactoe
+```
---
+++ Project-Tictactoe/tictactoe/__main__.py
@@ -0,0 +1,16 @@
+from tictactoe.game import Game
+from tictactoe.player import ConsolePlayer
+from tictactoe.io import ConsoleFrontend
+from tictactoe.io import TableConsoleFrontend
+
+
+def main():
+    #frontend = ConsoleFrontend()
+    frontend = TableConsoleFrontend()
+    player = ConsolePlayer('Doge')
+    game = Game(x_player=player, frontend=frontend)
+    game.play()
+
+
+if __name__ == '__main__':
+    main()
